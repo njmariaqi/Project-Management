577ab4fac403100ec79c6b77c71b0cc20322803c
Mengxiao Qi <mengxiaoqi@MacBook-Air.local>
2021-12-05 15:11:20 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,20 @@ require('dotenv').config(
      //{path: '../.env'}
 );
 
-const sequelize = new Sequelize(
+let sequelize;
+if (process.env.JAWSDB_URL) {
+     sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+     sequelize = new Sequelize(
      process.env.DB_NAME,
      process.env.DB_USER,
-     process.env.DB_PASS,
+     process.env.DB_PW,
      {
           host: 'localhost',
           dialect: 'mysql',
-          port: 3306
-     }
-);
+          port: 3306,
+     },
+     );
+}
 
 module.exports = sequelize;
